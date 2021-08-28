3d007f30713e5a22fb8fdc8959d23294ff8b3d67
Lofaloa <farci.logan@gmail.com>
2021-08-28 16:07:15 +0200

    Issue #3: the owner repository is created

---
+++ service/src/main/java/be/loganfarci/financial/service/owner/OwnerRepository.java
@@ -0,0 +1,6 @@
+package be.loganfarci.financial.service.owner;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<OwnerEntity, String> {
+}
